4e3bc3c8f4099a0b7b7e97b6221921c1a81b78f2
Agung Kurniawan <agung.kn21@gmail.com>
2022-03-28 14:37:36 +0700

    Add Course Model

---
+++ src/main/java/co/id/mii/mcc63lmsserverside/course/Course.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.id.mii.mcc63lmsserverside.course;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Agung
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String title;
+    
+    @Column(nullable = false)
+    private String description;
+    
+    @Column(nullable = false)
+    private Long price;
+    
+    private Boolean isActive;
+
+    private User user;
+    private Category category;
+}
