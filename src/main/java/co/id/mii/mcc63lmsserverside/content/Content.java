7fc1c8c599fe2c6ac44b3308dcf7cb97e9a968fe
Agung Kurniawan <72692058+Agung-Kn@users.noreply.github.com>
2022-03-29 11:35:34 +0700

    add content (#4)
    * Add Content Model
    
    * Add Content Repository
    
    * Add Content Service
    
    * Add Content Controller

---
+++ src/main/java/co/id/mii/mcc63lmsserverside/content/Content.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.id.mii.mcc63lmsserverside.content;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Agung
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Content {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String title;
+    
+    @Column(nullable = false)
+    private String description;
+    
+    @Column(nullable = false)
+    private String videoUrl;
+    
+    private Module module;
+}
