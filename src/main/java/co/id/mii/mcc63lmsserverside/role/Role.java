1597e5ce627afac624061a2fa52f3b2f60092712
Rizky Ardi Ilhami <45192318+ilhm-rai@users.noreply.github.com>
2022-03-29 11:12:59 +0700

    add role (#2)
    * Add Role Model
    
    * Add Role Repository
    
    * Add Role Service
    
    * Add Role Controller
    
    Co-authored-by: Agung Kurniawan <agung.kn21@gmail.com>

---
+++ src/main/java/co/id/mii/mcc63lmsserverside/role/Role.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.id.mii.mcc63lmsserverside.role;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Agung
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+}
