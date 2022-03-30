37d36f2ef6027433815231554f0eafc67b532cfc
Rizky Ardi Ilhami <45192318+ilhm-rai@users.noreply.github.com>
2022-03-30 14:08:10 +0700

    Add appropriate user response (#9)
    * create user model
    
    * create user repository
    
    * create user dto
    
    * add mappstruct and disable spring security
    
    * create user mapper
    
    * create user service
    
    * create user controller
    
    * Create user not found exception
    
    * Add appropriate response information

--- src/main/java/co/id/mii/mcc63lmsserverside/user/UserController.java
+++ src/main/java/co/id/mii/mcc63lmsserverside/user/UserController.java
@@ -3,6 +3,7 @@ package co.id.mii.mcc63lmsserverside.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,24 +27,37 @@ public class UserController {
   }
 
   @GetMapping
+  @ResponseBody
   public List<User> getUsers() {
     return userService.getUsers();
   }
 
-  @PostMapping
-  public void registerNewUser(@RequestBody UserDto userDto) {
-    userService.addNewUser(userDto);
+  @GetMapping("{userId}")
+  @ResponseBody
+  public User getUserById(@PathVariable("userId") Long userId) {
+    return userService.getUserById(userId);
   }
 
+  @PostMapping
+  @ResponseBody
+  @ResponseStatus(HttpStatus.CREATED)
+  public UserDto registerNewUser(@RequestBody UserDto userDto) {
+    return userService.addNewUser(userDto);
+  }
+
+  @DeleteMapping(path = "{userId}")
+  @ResponseStatus(HttpStatus.OK)
   @DeleteMapping(path = "{userId}")
   public void deleteUser(@PathVariable("userId") Long userId) {
     userService.deleteUser(userId);
   }
 
   @PutMapping(path = "{userId}")
-  public void updateUser(
+  @ResponseBody
+  @ResponseStatus(HttpStatus.CREATED)
+  public UserDto updateUser(
       @PathVariable("userId") Long userId, @RequestBody UserDto userDto) {
 
-    userService.updateUser(userId, userDto);
+    return userService.updateUser(userId, userDto);
   }
 }
