177b0fb70f77549dff583388606dc694aae6fada
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2022-04-07 11:28:32 +0700

    Add get payment slip file

--- src/main/java/co/id/mii/mcc63lmsserverside/controller/PaymentController.java
+++ src/main/java/co/id/mii/mcc63lmsserverside/controller/PaymentController.java
@@ -1,14 +1,20 @@
 package co.id.mii.mcc63lmsserverside.controller;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import co.id.mii.mcc63lmsserverside.model.Payment;
 import co.id.mii.mcc63lmsserverside.model.dto.request.PaymentRequest;
 import co.id.mii.mcc63lmsserverside.service.PaymentService;
+import co.id.mii.mcc63lmsserverside.util.StorageService;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -26,5 +32,13 @@ public class PaymentController {
   @PostMapping("{paymentId}/confirm")
   public String confirm(@PathVariable("paymentId") Long paymentId) {
     return paymentService.paid(paymentId);
+  }
+
+  @GetMapping("/files/{filename:.+}")
+  @ResponseBody
+  public ResponseEntity<Resource> getFile(@PathVariable("filename") String filename) {
+    Resource file = StorageService.loadAsResource("upload/payment", filename);
+    return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+        "attachment; filename=\"" + file.getFilename() + "\"").body(file);
   }
 }
