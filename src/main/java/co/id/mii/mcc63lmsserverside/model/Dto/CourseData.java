f7d0d077e2672750a498c5a1ea96fa0ca84e6b5c
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2022-04-07 14:19:55 +0700

    Add course image

--- src/main/java/co/id/mii/mcc63lmsserverside/model/Dto/CourseData.java
+++ src/main/java/co/id/mii/mcc63lmsserverside/model/Dto/CourseData.java
@@ -1,10 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * To change this license header, choose License Headers in Project Properties. To change this
+ * template file, choose Tools | Templates and open the template in the editor.
  */
 package co.id.mii.mcc63lmsserverside.model.dto;
 
+import org.springframework.web.multipart.MultipartFile;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,4 +24,5 @@ public class CourseData {
     private Long price;
     private Long userId;
     private Long categoryId;
+    private MultipartFile courseImage;
 }
