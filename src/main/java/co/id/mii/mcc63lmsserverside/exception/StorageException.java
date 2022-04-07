6eed13a37f907891c67ca1aba0d592bb6d767854
Rizky Ardi Ilhami <rizkyardi.ilhami06@gmail.com>
2022-04-07 11:27:03 +0700

    Update file handler

---
+++ src/main/java/co/id/mii/mcc63lmsserverside/exception/StorageException.java
@@ -0,0 +1,12 @@
+package co.id.mii.mcc63lmsserverside.exception;
+
+public class StorageException extends RuntimeException {
+
+  public StorageException(String message) {
+    super(message);
+  }
+
+  public StorageException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
---
+++ src/main/java/co/id/mii/mcc63lmsserverside/exception/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package co.id.mii.mcc63lmsserverside.exception;
+
+public class StorageFileNotFoundException extends StorageException {
+
+  public StorageFileNotFoundException(String message) {
+    super(message);
+  }
+
+  public StorageFileNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
